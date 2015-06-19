e55a18c783d9144de0ae7da5fbb42855828af682
jirik <jirik@users.noreply.github.com>
2015-06-19 13:37:45 +0200

    current config.js and templates from the web

--- client/src/config.js
+++ client/src/config.js
@@ -14,6 +14,20 @@ mapseries.config = {
           return stringValue;
         }
       }
+    },
+    {
+      title: "Austria-Hungary: 3rd Military Survey, 1:200 K (RDA 2015)",
+      layer: "austria-hungary--3rd-military-survey--200",
+      template: "ah-ms3-200_rda2015.txt",
+      formatFunctions: {
+        addDegrees: function(stringValue, sheet) {
+          stringValue += '';
+          if(stringValue.length==4) {
+            stringValue = stringValue.substr(0,2)+'°'+stringValue.substr(2)+'°';
+          }
+          return stringValue;
+        }
+      }
     }
   ]
 }
