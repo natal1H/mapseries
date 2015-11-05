42fc9b5166e3c584862bf0092f80c54b36436912
Erich Duda <dudaerich@gmail.com>
2015-11-05 09:51:27 +0100

    Added formatFunctionsTemplate into the config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,14 @@
 var mapseries = {};
 mapseries.config = {
+  formatFunctionsTemplate: {
+    addDegrees: function(stringValue, sheet) {
+      stringValue += '';
+      if(stringValue.length==4) {
+        stringValue = stringValue.substr(0,2)+'°'+stringValue.substr(2)+'°';
+      }
+      return stringValue;
+    }
+  },
   series: [
     {
       title: "Austria-Hungary: 3rd Military Survey, 1:200 K",
