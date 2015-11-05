b6c929588afc79fd72ca3d1fc459d4bb2c509ab5
Erich Duda <dudaerich@gmail.com>
2015-11-05 13:27:09 +0100

    Clenead up data

--- client/src/config.js
+++ client/src/config.js
@@ -11,25 +11,11 @@ mapseries.config = {
   },
   series: [
     {
-      title: "Austria-Hungary: 3rd Military Survey, 1:200 K",
-      layer: "austria-hungary--3rd-military-survey--200",
-      template: "ah-ms3-200.txt",
+      title: "Austria-Hungary: Austria-Hungary: 3rd Military Survey, 1:200 K",
+      layer: "austria-hungary-austria-hungary-3rd-military-survey-1200-k",
+      template: "austria-hungary-austria-hungary-3rd-military-survey-1200-k.txt",
       formatFunctions: {
-        addDegrees: function(stringValue, sheet) {
-          stringValue += '';
-          if(stringValue.length==4) {
-            stringValue = stringValue.substr(0,2)+'°'+stringValue.substr(2)+'°';
-          }
-          return stringValue;
-        }
-      }
-    },
-    {
-      title: "Austria-Hungary: 3rd Military Survey, 1:200 K (RDA 2015)",
-      layer: "austria-hungary--3rd-military-survey--200",
-      template: "ah-ms3-200_rda2015.txt",
-      formatFunctions: {
-        addDegrees: function(stringValue, sheet) {
+        addDegrees: function (stringValue, sheet) {
           stringValue += '';
           if(stringValue.length==4) {
             stringValue = stringValue.substr(0,2)+'°'+stringValue.substr(2)+'°';
