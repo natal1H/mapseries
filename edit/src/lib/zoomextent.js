614bbceaff472af2508cff695799968caaa9cd9b
Erich Duda <dudaerich@gmail.com>
2017-08-17 22:46:58 +0200

    Fix issue in zoomextent.js
    If the GeoJSON is empty, turf returns
    bbox with infinity values which is
    not accepted by fitBounds method.
    In this case the method shouldn't be
    called.

--- edit/src/lib/zoomextent.js
+++ edit/src/lib/zoomextent.js
@@ -1,8 +1,10 @@
+var $ = require('jquery');
+
 module.exports = function(context) {
     // var bounds = context.mapLayer.getBounds();
     // if (bounds.isValid()) context.map.fitBounds(bounds);
     var bbox = turf.bbox(context.data.get('map'));
-    if (bbox) {
+    if (bbox && !$.inArray(Infinity, bbox) && !$.inArray(-Infinity, bbox)) {
       context.map.fitBounds(bbox);
     }
 };
