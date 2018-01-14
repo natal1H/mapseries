2554542610d238e0a2431e038f31190f0ff23f1b
Erich Duda <dudaerich@gmail.com>
2018-01-14 13:10:48 +0100

    Updated version of github-api

--- edit/src/config.js
+++ edit/src/config.js
@@ -7,7 +7,7 @@ module.exports = function(hostname) {
     };
   // Customize these settings for your own development/deployment
   // version of geojson.io.
-} else if (hostname === 'mapseries.devel.mzk.cz') {
+  } else if (hostname === 'mapseries.devel.mzk.cz') {
     return {
       client_id: '5395a41f086b3fcaa499',
       gatekeeper_url: 'http://mapseries.devel.mzk.cz/server',
